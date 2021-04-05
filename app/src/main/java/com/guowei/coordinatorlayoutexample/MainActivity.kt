82744f5148337331a8a5ce76223719d309e76570
Guowei Lv <lvguowei1002@gmail.com>
2021-04-05 14:48:35 +0300

    simple behavior

--- app/src/main/java/com/guowei/coordinatorlayoutexample/MainActivity.kt
+++ app/src/main/java/com/guowei/coordinatorlayoutexample/MainActivity.kt
@@ -1,30 +1,19 @@
 package com.guowei.coordinatorlayoutexample
 
-import android.graphics.Rect
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val adapter = SimpleAdapter()
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.addItemDecoration(object : RecyclerView.ItemDecoration() {
-            override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
-                val position = parent.getChildViewHolder(view).adapterPosition
-                val offset = parent.resources.getDimensionPixelOffset(R.dimen.activity_vertical_margin)
-                outRect.set(offset, if (position == 0) offset else 0, offset, offset)
-            }
-        })
+        val simpleButton = findViewById<Button>(R.id.simple)
+        simpleButton.setOnClickListener {
+            val intent = Intent(this, SimpleActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
